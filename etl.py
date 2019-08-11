7fd9ef6960c0524d2547e77e3446ce9fa71181c0
Varun Sharma <varunvsharma@gmail.com>
2019-08-11 20:58:13 +1000

    Add function for processing song files

--- etl.py
+++ etl.py
@@ -1,3 +1,21 @@
+import os
+import glob
+import psycopg2
+import pandas as pd
+from sql_queries import *
+
+def process_song_file(cur, filepath):
+    # open song file
+    df = pd.read_json(filepath, lines=True)
+
+    # insert song record
+    song_data = list(df[['song_id','title','artist_id','year','duration']].values[0])
+    cur.execute(song_table_insert, song_data)
+
+    # insert artist record
+    artist_data = list(df[['artist_id','artist_name','artist_location','artist_latitude','artist_longitude']].values[0])
+    cur.execute(artist_table_insert, artist_data)
+
 def main():
 
 if __name__ == "__main__":
