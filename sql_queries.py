e6167d00da95183a810b2499c9bbe0105df5c8ed
Varun Sharma <varunvsharma@gmail.com>
2019-08-11 20:43:13 +1000

    Add comment indicating sql query lists

--- sql_queries.py
+++ sql_queries.py
@@ -69,5 +69,7 @@ song_select = ("""SELECT song_id, songs.artist_id
                   ON songs.artist_id = artists.artist_id
                   WHERE title = (%s) AND artist_name = (%s) AND duration = (%s)""")
 
+# QUERY LISTS
+
 create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
