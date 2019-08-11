4cfdd8d5f9b69fb5b6852729dd974796c3b70405
Varun Sharma <varunvsharma@gmail.com>
2019-08-11 20:23:50 +1000

    Add function for creating tables

--- create_tables.py
+++ create_tables.py
@@ -1,5 +1,6 @@
 import psycopg2
-from sql_queries import drop_table_queries
+from sql_queries import create_table_queries, drop_table_queries
+
 
 def create_database():
     # connect to default database
@@ -20,17 +21,27 @@ def create_database():
 
     return cur, conn
 
+
 def drop_tables(cur, conn):
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
+
+def create_tables(cur, conn):
+    for query in create_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
 def main():
     cur, conn = create_database()
 
     drop_tables(cur, conn)
+    create_tables(cur, conn)
 
     conn.close()
+
 
 if __name__ == "__main__":
     main()
