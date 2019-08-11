28e23e4f2ee5ceb95bc1c9ed68981455f627c178
Varun Sharma <varunvsharma@gmail.com>
2019-08-11 21:06:49 +1000

    Add processing function calls

--- etl.py
+++ etl.py
@@ -78,8 +78,17 @@ def process_data(cur, conn, filepath, func):
         func(cur, datafile)
         conn.commit()
         print('{}/{} files processed.'.format(i, num_files))
-        
+
+
 def main():
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    cur = conn.cursor()
+
+    process_data(cur, conn, filepath='data/song_data', func=process_song_file)
+    process_data(cur, conn, filepath='data/log_data', func=process_log_file)
+
+    conn.close()
+
 
 if __name__ == "__main__":
     main()
