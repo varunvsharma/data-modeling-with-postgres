4b1c1c9b86eda588696ccd0cacbd8f04b70c008b
Varun Sharma <varunvsharma@gmail.com>
2019-08-11 20:03:42 +1000

    Add function for creating the database

--- create_tables.py
+++ create_tables.py
@@ -1,4 +1,28 @@
+import psycopg2
+
+def create_database():
+    # connect to default database
+    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
+    conn.set_session(autocommit=True)
+    cur = conn.cursor()
+
+    # create sparkify database with UTF8 encoding
+    cur.execute("DROP DATABASE IF EXISTS sparkifydb")
+    cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
+
+    # close connection to default database
+    conn.close()
+
+    # connect to sparkify database
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    cur = conn.cursor()
+
+    return cur, conn
+
 def main():
+    cur, conn = create_database()
+
+    conn.close()
 
 if __name__ == "__main__":
     main()
