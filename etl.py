cf21e1d8243c508c56f706042bf045ad576525e9
Varun Sharma <varunvsharma@gmail.com>
2019-08-11 21:02:04 +1000

    Add function for processing log files

--- etl.py
+++ etl.py
@@ -4,6 +4,7 @@ import psycopg2
 import pandas as pd
 from sql_queries import *
 
+
 def process_song_file(cur, filepath):
     # open song file
     df = pd.read_json(filepath, lines=True)
@@ -15,6 +16,49 @@ def process_song_file(cur, filepath):
     # insert artist record
     artist_data = list(df[['artist_id','artist_name','artist_location','artist_latitude','artist_longitude']].values[0])
     cur.execute(artist_table_insert, artist_data)
+
+
+def process_log_file(cur, filepath):
+    # open log file
+    df = pd.read_json(filepath, lines=True)
+
+    # filter by NextSong action
+    df = df[df.page == 'NextSong']
+
+    # convert timestamp column to datetime
+    t = pd.to_datetime(df.ts, unit='ms')
+
+    # insert time data records
+    time_data = (list(df.ts), list(t.dt.hour), list(t.dt.day), list(t.dt.weekofyear), list(t.dt.month), \
+                 list(t.dt.year), list(t.dt.weekday))
+    column_labels = ('timestamp', 'hour', 'day', 'weekofyear', 'month', 'year', 'weekday')
+    time_df = pd.DataFrame.from_dict({column_labels[i]:time_data[i] for i in range(len(column_labels))})
+
+    for i, row in time_df.iterrows():
+        cur.execute(time_table_insert, list(row))
+
+    # load user table
+    user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level']]
+
+    # insert user records
+    for i, row in user_df.iterrows():
+        cur.execute(user_table_insert, row)
+
+    # insert songplay records
+    for index, row in df.iterrows():
+
+        # get songid and artistid from song and artist tables
+        cur.execute(song_select, (row.song, row.artist, row.length))
+        results = cur.fetchone()
+
+        if results:
+            songid, artistid = results
+        else:
+            songid, artistid = None, None
+
+        # insert songplay record
+        songplay_data = (row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
+        cur.execute(songplay_table_insert, songplay_data)
 
 def main():
 
